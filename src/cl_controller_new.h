ff8348c8ab1f937d070cbd6cc0a3cd15241aca59
Bernardo <bernardo.knackfuss@gmail.com>
2021-10-26 18:51:11 -0300

    clean up collision controller code

--- src/cl_controller_new.h
+++ src/cl_controller_new.h
@@ -14,54 +14,23 @@ void CL_run_collision_detection(Entity* entityA, Player* player)
 
    if(box_gjk_test.collision)
    {
-      // if(G_SCENE_INFO.tmp_unstuck_things)
-      // {
-         EPA_Result box_epa_test = CL_run_EPA(box_gjk_test.simplex, &box_collider_A, &box_collider_B);
-         
-         if(box_epa_test.collision)
+      EPA_Result epa = CL_run_EPA(box_gjk_test.simplex, &box_collider_A, &box_collider_B);
+      
+      if(epa.collision)
+      {
+         RENDER_MESSAGE("Penetration: " + format_float_tostr(epa.penetration, 4), 1);
+
+         RENDER_MESSAGE(to_str(player_entity->position), 1);
+
+         if(IM_Values.btn2)
          {
-            RENDER_MESSAGE("Penetration: " + format_float_tostr(box_epa_test.penetration, 4), 1);
+            float old_z = player_entity->position.z;
+            player_entity->position += epa.direction * epa.penetration;
+            //player->brute_stop();
+            CL_wall_slide_player(player, epa);
 
-            RENDER_MESSAGE(to_str(player_entity->position), 1);
-
-            if(IM_Values.btn2)
-            {
-               float old_z = player_entity->position.z;
-               player_entity->position += box_epa_test.direction * box_epa_test.penetration;
-               player->brute_stop();
-
-               // RENDER_MESSAGE("STUCK OUT: " + format_float_tostr(box_epa_test.penetration, 4) + "", 2000);
-               // IM_Values.btn2 = false;
-               // if(!IM_Values.btn)
-               // {
-               //    float diff_z = player_entity->position.z - old_z;
-               //    RENDER_MESSAGE("ITERATION: " + to_string(IM_Values.val_int++) + " dif: " + fmt_tostr(diff_z, 4), 1000);
-
-               // }
-
-               player_entity->update();
-            }
-
-
-            RENDER_MESSAGE(to_str(player_entity->position), 1);
-
-            vec3 startpoint = entityA->position + vec3(entityA->scale.x / 2.0, entityA->scale.y + 0.2, entityA->scale.z / 2.0);
-            vec3 endpoint   = startpoint + box_epa_test.direction * entityA->scale.x / 2.0f;
-            vec3 penetration_point = endpoint - box_epa_test.direction * box_epa_test.penetration;
-
-            // IM_RENDER.add_line(IMHASH, startpoint, endpoint, 2, true, vec3(0.96,0.24,0.24));
-            // IM_RENDER.add_point(IMHASH, startpoint, 3, true, vec3(0, 0.24, 0.96));
-            // IM_RENDER.add_point(IMHASH, endpoint, 3, true, vec3(0, 0.96, 0.24));
-
-            IM_RENDER.add_line(IMHASH, endpoint, penetration_point, 2, true, vec3(0.96,0.24,0.24));
+            player_entity->update();
          }
-         else
-         {
-            RENDER_MESSAGE("No Penetration Found!", 2000);
-         }
-
-         //G_SCENE_INFO.tmp_unstuck_things = false;
-      //}  
+      }
    }
-   // RENDER_MESSAGE(box_gjk_test.collision ? "Collision!!" : "No Collision!" );
 }
