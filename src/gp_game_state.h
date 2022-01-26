26b7f4a4f8dbe04199f8c8d578dfac3cf5706297
Bernardo <bernardo.knackfuss@gmail.com>
2022-01-26 16:56:55 -0300

    wip timer target work

--- src/gp_game_state.h
+++ src/gp_game_state.h
@@ -1,51 +1,29 @@
 
 struct GameState {
 
-   // timed event
-   bool     timer_active            = false;
-   Entity*  timer_target            = nullptr;
-   float    timer_remaining_time    = 0;
-
-   // events
-   u32 events;
+   // timed events
+   u32         timer_targets_array_size = 64;
+   TimerTarget timer_targets[timer_targets_array_size];
 
 } Game_State;
 
 
 // prototypes
-void GP_update_game_state();
 void GP_game_state_stop_timer();
 void GP_game_state_start_timer(Entity* interactable);
 
 
 
-void GP_update_game_state()
-{
-   if(Game_State.timer_active)
-   {
-      Game_State.timer_remaining_time -= G_FRAME_INFO.duration;
-      if(Game_State.timer_remaining_time <= 0)
-         GP_game_state_stop_timer();
-   }
-}
-
-
 void GP_game_state_stop_timer()
 {
    Game_State.timer_active = false;
-   Game_State.timer_target->timer_end_action = true;
+   Game_State.timer_target->timer_end = true;
 }
 
 
 void GP_game_state_start_timer(Entity* interactable)
 {
-   if(Game_State.timer_target == nullptr)
-      return;
-      
-   Game_State.timer_remaining_time              = (float) interactable->timer_duration;
-   Game_State.timer_target                      = interactable->timer_target;
-   Game_State.timer_active                      = true;
-   Game_State.timer_target->timer_end_action    = true;
+
 }
 
 
